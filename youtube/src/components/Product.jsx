a584e01ca30562efee7055a70d5e06cebb53ac0e
nguyenthevu1 <nguyenthevu2602@gmail.com>
2021-11-22 21:52:41 +0700

    complete home page

---
+++ youtube/src/components/Product.jsx
@@ -0,0 +1,88 @@
+import styled from 'styled-components';
+import {
+    SearchOutlined,
+    ShoppingCartOutlined,
+    FavoriteBorderOutlined,
+} from '@material-ui/icons';
+
+const Info = styled.div`
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.2);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.7s ease;
+    z-index: 3;
+`;
+const Container = styled.div`
+    flex: 1;
+    margin: 5px;
+    min-width: 280px;
+    height: 350px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #f5fbfd;
+    position: relative;
+    &:hover ${Info} {
+        opacity: 1;
+    }
+`;
+
+const Circle = styled.div`
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background-color: white;
+    position: absolute;
+`;
+const Image = styled.img`
+    height: 75%;
+    width: 100%;
+    object-fit: cover;
+    z-index: 2;
+`;
+
+const Icon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+    transition: all 0.3s ease;
+    &:hover {
+        background-color: #e9f5f5;
+        transform: scale(1.1);
+    }
+    cursor: pointer;
+`;
+
+const Product = ({ item }) => {
+    return (
+        <Container>
+            <Circle />
+            <Image src={item.img} />
+            <Info>
+                <Icon>
+                    <ShoppingCartOutlined />
+                </Icon>
+                <Icon>
+                    <SearchOutlined />
+                </Icon>
+                <Icon>
+                    <FavoriteBorderOutlined />
+                </Icon>
+            </Info>
+        </Container>
+    );
+};
+
+export default Product;
---
+++ youtube/src/data.js
@@ -0,0 +1,60 @@
+export const sliderItem = [
+    {
+        id: 1,
+        img: '',
+        title: 'WINTER SALE',
+        desc: 'DON"T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.',
+        bg: 'f5fafd',
+    },
+    {
+        id: 2,
+        img: '',
+        title: 'SUMMER SALE',
+        desc: 'DON"T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.',
+        bg: 'fcf1ed',
+    },
+    {
+        id: 3,
+        img: '',
+        title: 'POPULAR SALE',
+        desc: 'DON"T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.',
+        bg: 'fbf0f4',
+    },
+];
+
+export const CategoryItems = [
+    {
+        id: 1,
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFSjbG9wNKx99z6Fszxr78iPvnJEjCjMurxw&usqp=CAU',
+        title: 'SHIRT STYLE!',
+    },
+    {
+        id: 2,
+        img: 'https://giaycaosmartmen.com/wp-content/uploads/2020/09/giay-sneaker-la-gi.jpg',
+        title: 'LOUNGEWEAR LOVE!',
+    },
+    {
+        id: 3,
+        img: 'https://giaycaosmartmen.com/wp-content/uploads/2020/09/giay-sneaker-la-gi.jpg',
+        title: 'LIGHT JACKETS!',
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: 'https://giaycaosmartmen.com/wp-content/uploads/2020/09/giay-sneaker-la-gi.jpg',
+    },
+    {
+        id: 2,
+        img: 'https://giaycaosmartmen.com/wp-content/uploads/2020/09/giay-sneaker-la-gi.jpg',
+    },
+    {
+        id: 3,
+        img: 'https://giaycaosmartmen.com/wp-content/uploads/2020/09/giay-sneaker-la-gi.jpg',
+    },
+    {
+        id: 4,
+        img: 'https://giaycaosmartmen.com/wp-content/uploads/2020/09/giay-sneaker-la-gi.jpg',
+    },
+];
